cb278cec97569a3f6f61e7ee595360751b383aa3
Mrjane888 <anat.srii@gmail.com>
2024-09-16 23:37:18 +0700

    Re-learn javascript loop function

---
+++ javascript/function.js
@@ -0,0 +1,12 @@
+function changeMessage() {
+    document.getElementById("hcolor").innerText = "Hello Sukar";
+}
+
+function alertMessage () {
+    alert("Sukar is my cat");
+}
+
+function changeColor () {
+    document.getElementById("hcolor").style.color = "green";
+}
+
